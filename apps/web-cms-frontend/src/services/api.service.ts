e5e5985e4140a8d8451c45b12ea2bae2ff8cee17
hiep.nguyen <hiep.nguyen@itech.asia>
2025-10-10 15:11:01 +0700

    login

--- apps/web-cms-frontend/src/services/api.service.ts
+++ apps/web-cms-frontend/src/services/api.service.ts
@@ -2,5 +2,5 @@ import { HTTP } from "@blog-frontend/core";
 import { ILoginData, IResponse } from "@blog-frontend/shared";
 
 export const login = (identifier: string, password: string): Promise<IResponse<ILoginData>> => {
-    return HTTP.post('user/users/login', { identifier, password });
+    return HTTP.post('auth/login', { identifier, password });
 }
